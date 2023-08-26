d83bd67a28bb04bc822bd4d24380451b3ec94a4e
luqu1t5 <lucadalto@hotmail.com>
2023-08-25 23:50:51 -0300

    Agregado index.html y script.js

---
+++ Desktop/Trabajando con Arrays/js/script.js
@@ -0,0 +1,40 @@
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  array.forEach((element) => {
+    if (typeof element === "string") {
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(element));
+      container.appendChild(li);
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  showList(strangeArray);
+});
